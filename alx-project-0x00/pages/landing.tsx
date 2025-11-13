0a382f56a5ce20bf80b30cd0faf980e205630779
dijez <khadijaahmad2k21@gmail.com>
2025-11-13 19:03:22 +0100

    update

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,7 +7,7 @@ const Landing: React.FC = () => {
         <h1 className=" text-xl font-extralight">Landing Page</h1>
         <Button title="small rounded" styles="text-sm rounded-sm" />
         <Button title="meduim rounded" styles="text-sm rounded-md" />
-        <Button title="large rounded" styles="text-sm rounded-full" />
+        <Button title="large rounded" styles="text-sm rounded-lg" />
         <Card />
         <Card />
         <Card />
