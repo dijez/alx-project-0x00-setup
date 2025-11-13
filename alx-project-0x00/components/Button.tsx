513a6da6cffe7962fd6b1e4e5b3fa1bb10dcfa9c
dijez <khadijaahmad2k21@gmail.com>
2025-11-13 18:59:06 +0100

    update

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ title, styles })  => {
+    return(
+        <button
+         className= { `px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 transition ${styles}`}
+        > {title}
+        
+        </button>
+    )
+}
+
+export default Button
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,13 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button"
 
 const Landing: React.FC = () => {
     return (
         <div>
         <h1 className=" text-xl font-extralight">Landing Page</h1>
+        <Button title="small rounded" styles="text-sm rounded-sm" />
+        <Button title="meduim rounded" styles="text-sm rounded-md" />
+        <Button title="large rounded" styles="text-sm rounded-full" />
         <Card />
         <Card />
         <Card />
