315bc0c867b7aac067b02c8d580a74d7e6169279
dijez <khadijaahmad2k21@gmail.com>
2025-11-13 11:52:00 +0100

    update

---
+++ alx-project-0x00/pages/about.tsx
@@ -0,0 +1,11 @@
+const About: React.FC = () => {
+    return(
+        <div>
+            <h1 className="text-xl font-extralight">About
+
+            </h1>
+        </div>
+    )
+}
+
+export default About
