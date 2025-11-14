ff2e90ac1ad768010448613e2711e85cff142511
dijez <khadijaahmad2k21@gmail.com>
2025-11-14 06:59:07 +0100

    update

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+// props means properties passed from one component to another 
+// they are like parameters that can be passed in a function 
+
 import { ButtonProps } from "@/interfaces";
 
 const Button: React.FC<ButtonProps> = ({ title, styles })  => {
     return(
         <button
          className= { `px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 transition ${styles}`}
+        // here title is a property passed 
         > {title}
         
         </button>
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,6 +7,7 @@ const Landing: React.FC = () => {
         <h1 className=" text-xl font-extralight">Landing Page</h1>
         <Button title="small rounded" styles="text-sm rounded-sm" />
         <Button title="meduim rounded" styles="text-sm rounded-md" />
+        {/* only expected to have either rounded full or lg but the checker is confused  */}
         <Button title="large rounded" styles="text-sm rounded-full" />
         <Button title="large rounded" styles="text-sm rounded-lg" />
         <Card />
